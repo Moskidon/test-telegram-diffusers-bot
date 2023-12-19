da42d8db00eb02a7b288fcdaf52c6a08c75eb49a
Moskidon <46126524+Moskidon@users.noreply.github.com>
2023-12-19 16:24:19 +0500

    staff

--- data_config.js
+++ data_config.js
@@ -3,8 +3,8 @@ const images = [
         title: 'SD XL TURBO',
         description: "SDXL Turbo is an adversarial time-distilled Stable Diffusion XL (SDXL) model capable of running inference in as little as 1 step",
         configurableParams: {
-            "Images Count": {value: 4, step: 4, min: 4, max: 32},
-            "Inference Steps": {value: 1, step: 1, min: 1, max: 5},
+            "Images Count": {value: 8, step: 4, min: 4, max: 32},
+            "Inference Steps": {value: 1, step: 1, min: 1, max: 8},
         },
         samples: ["https://image.civitai.tech/xG1nkqKTMzGDvpLrqFT7WA/d3f4288b-9442-42a6-abf6-d97f8ebe013e/width=450/00588-2023-11-28-2676931633.jpeg",
             "https://image.civitai.tech/xG1nkqKTMzGDvpLrqFT7WA/ee8a2876-3be4-4571-8af8-1464869d43bb/width=450/00931-2023-11-28-4073367246.jpeg",
@@ -70,5 +70,44 @@ const images = [
             "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/827f39dc-cee8-49a5-8967-2c5ce91c9cba/width=450/31073308-2065889259-Photography,%20full%20body%20of%20real%20life%20pikachu%20portrait,%20symmetrical,%20highly%20detailed,%20smooth,%20sharp%20focus,%20cinematic%20lighting,%20kkw.jpeg",
             "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/d965efdb-30c7-4c01-9fe1-f2de0712fd9d/width=450/31073305-2065889259-macro%20camera,%20illustration%20of%20a%20little%20magical%20cute%20forest%20creature.%20The%20creature%20IS%20holding%20a%20spear.%20fantasy%20art,%20intricate%20det.jpeg"
         ]
+    },
+    {
+        title: 'ZavyChromaXL',
+        description: "",
+        configurableParams: {
+            "Images Count": {value: 4, step: 4, min: 4, max: 16},
+            "Inference Steps": {value: 25, step: 1, min: 1, max: 50},
+            "Guidance Scale": {value: 7.5, step: 0.5, min: 0, max: 20}
+        },
+        samples: ["https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/c4c30afb-8d9d-4aa5-a8ba-7f7670e059b2/width=450/ChromaXL_00037.jpeg",
+            "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/9dd1ece0-3325-470f-b004-5f611b2b3d3c/width=450/ChromaXL_00076.jpeg",
+            "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/8c8f23b8-59a8-4cbb-88b9-6cbecde062b8/width=450/ChromaXL_00054.jpeg",
+            "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/72a12032-770e-46ee-ac7b-d408b3c90920/width=450/ChromaXL_00082.jpeg",
+            "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/e87f3cae-a882-4891-8b2d-3f642d9e9213/width=450/ChromaXL_00014.jpeg",
+        ]
+    },
+    {
+        title: 'TurboVisionXL',
+        description: "Super Fast XL based on new SDXL Turbo - 3 - 5 step quality output at high resolutions!",
+        configurableParams: {
+            "Images Count": {value: 8, step: 4, min: 4, max: 32},
+            "Inference Steps": {value: 1, step: 1, min: 1, max: 8},
+        },
+        samples: ["https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/98c47ca0-b2fc-4861-ae82-b8c63e777b29/width=450/01404-2023-12-08-1291191819.jpeg",
+            "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/67f61de0-ac22-4285-8486-3940848b8645/width=450/00463-2023-12-08-182778452.jpeg",
+            "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/40d2c72d-b6bf-4d49-baf3-e2872e1ba8ab/width=450/00313-2023-12-08-3957678817.jpeg",
+            "https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7WA/ee455afe-14ee-48c3-a4b8-f88caceaea0f/width=450/00295-2023-12-08-2403540858.jpeg",
+        ]
     }
+];
+
+samplers = [
+    "DPM++ 2M Karras",
+    "DPM++ 2M SDE",
+    "Euler a",
+    "Euler",
+    "Heun",
+    "LMS",
+    "DPM2 Karras",
+    "DPM2 a Karras",
 ];
